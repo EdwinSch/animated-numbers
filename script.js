6cacc63ec802b38717de7729e49a3e7103781531
front-ed <edwinschutjes@gmail.com>
2023-03-19 12:23:41 +0100

    final functionality and deployment

--- script.js
+++ script.js
@@ -8,8 +8,21 @@ const updateCount = (element) => {
   const value = parseInt(element.dataset.value);
   const increment = Math.ceil(value / 1000);
   let initialValue = 0;
+
+  const increaseCount = setInterval(() => {
+    initialValue += increment;
+    // break out of counter if number is reached
+    if (initialValue > value) {
+      element.textContent = `${value}+`;
+      clearInterval(increaseCount);
+      return;
+    }
+    // display count
+    element.textContent = `${initialValue}+`;
+  }, 1);
 };
 
+// Invoke update counter on each number element
 getNumbers.forEach((number) => {
   updateCount(number);
 });
